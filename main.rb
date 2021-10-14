eca257d30bb0da9c2418809019232395a110e8a0
kyl graym <kylemaxgray@gmail.com>
2021-10-14 17:22:29 +0000

    initial planning

--- main.rb
+++ main.rb
@@ -1,1 +1,5 @@
-
+# need a Game, Player and Question classes 
+# Game tracks 1) the game status, is_running? 2) scored 3) imports players
+# Players have 1) name 2)lives - in our case 3lives
+# Question generates two random numbers, string question and answer as sum of numbers
+ 
